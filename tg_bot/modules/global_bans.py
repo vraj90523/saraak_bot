661cdb0b35e9828c87755e5d9a2c70b09814c7e9
YasiR Siddiqui <38703416+Yasir-siddiqui@users.noreply.github.com>
2018-06-26 03:06:29 +0530

    Improved
    Improved Global bans strings ( ⚆ _ ⚆ )

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -91,7 +91,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
 
         return
 
-    message.reply_text("*Blows dust off of banhammer* 😉")
+    message.reply_text("*Why so serious* ( ⚆ _ ⚆ )")
 
     banner = update.effective_user  # type: Optional[User]
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
@@ -124,7 +124,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
             pass
 
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS, "gban complete!")
-    message.reply_text("Person has been gbanned.")
+    message.reply_text("Person has been gbanned ಠ_ಥ")
 
 
 @run_async
@@ -142,7 +142,7 @@ def ungban(bot: Bot, update: Update, args: List[str]):
         return
 
     if not sql.is_user_gbanned(user_id):
-        message.reply_text("This user is not gbanned!")
+        message.reply_text("This user is not gbanned ｡◕‿‿◕｡")
         return
 
     banner = update.effective_user  # type: Optional[User]
@@ -181,7 +181,7 @@ def ungban(bot: Bot, update: Update, args: List[str]):
 
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS, "un-gban complete!")
 
-    message.reply_text("Person has been un-gbanned.")
+    message.reply_text("Person has been un-gbanned (^̮^)")
 
 
 @run_async
