796d8190e7673e4ac69f90d519c3539e6bd3373b
YasiR Siddiqui <38703416+Yasir-siddiqui@users.noreply.github.com>
2018-06-26 01:13:39 +0530

    Personalisations
    Improved Start strings
    Changed donation link!
    Nothing else :v

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,17 +20,19 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
 
-I'm a group manager bot maintained by [this wonderful person](tg://user?id={}). I'm built in python3, using the \
+I'm a group manager bot maintained by [this noob](tg://user?id={}). I'm built in python3, using the \
 python-telegram-bot library, and am fully opensource - you can find what makes me tick \
-[here](github.com/PaulSonOfLars/tgbot)!
+[here](github.com/Yasir-siddiqui/Joker_dabot)!
 
-Feel free to submit pull requests on github, or to contact my support group, @MarieSupport, with any bugs, questions \
+Feel free to submit pull requests on github, or contact [my owner](t.me/Yasirsiddiqui), with any bugs, questions \
 or feature requests you might have :)
-I also have a news channel, @MarieNews for announcements on new features, downtime, etc.
+I also have a news channel, @JokerHarleynews for announcements on new features, downtime, etc.
 
 You can find the list of available commands with /help.
 
 If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
+
+[Click here to add me in your groups](https://telegram.me/Joker_dabot?startgroup=add)
 """
 
 HELP_STRINGS = """
@@ -52,10 +54,8 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
-motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
-(see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+If you want to donate, PM [YasiR Siddiqui](t.me/Yasirsiddiqui), and ask for donate. \
+If you really want to donate, it could really help me to upgrade my VPS."""
 
 IMPORTED = {}
 MIGRATEABLE = []
@@ -147,7 +147,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN)
     else:
-        update.effective_message.reply_text("Yo, whadup?")
+        update.effective_message.reply_text("Sar, I'm alive :D")
 
 
 # for test purposes
@@ -377,7 +377,7 @@ def donate(bot: Bot, update: Update):
         update.effective_message.reply_text(DONATE_STRING, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
         if OWNER_ID != 254318997 and DONATION_LINK:
-            update.effective_message.reply_text("You can also donate to the person currently running me "
+            update.effective_message.reply_text("You can also donate to the main creator of bot "
                                                 "[here]({})".format(DONATION_LINK),
                                                 parse_mode=ParseMode.MARKDOWN)
 
@@ -385,7 +385,7 @@ def donate(bot: Bot, update: Update):
         try:
             bot.send_message(user.id, DONATE_STRING, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
-            update.effective_message.reply_text("I've PM'ed you about donating to my creator!")
+            update.effective_message.reply_text("I've PM'ed you about donating to my creator :D")
         except Unauthorized:
             update.effective_message.reply_text("Contact me in PM first to get donation information.")
 
