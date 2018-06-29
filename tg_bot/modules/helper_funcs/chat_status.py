70df24da376975676656120893476efdbc8834a9
Ali Hasan <36104227+AliHasan96@users.noreply.github.com>
2018-06-29 16:39:50 +0500

    Make it little polite

--- tg_bot/modules/helper_funcs/chat_status.py
+++ tg_bot/modules/helper_funcs/chat_status.py
@@ -121,7 +121,7 @@ def user_admin(func):
             update.effective_message.delete()
 
         else:
-            update.effective_message.reply_text("Who dis non-admin telling me what to do?")
+            update.effective_message.reply_text("Only admins are allowed to perform this action")
 
     return is_admin
 
