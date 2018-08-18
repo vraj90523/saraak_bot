6f405bf30c3bb36075f29c2f08a18e674012ff00
YasiR Siddiqui <38703416+Yasir-siddiqui@users.noreply.github.com>
2018-08-18 14:08:23 +0530

    Don't gkick secret sudo user
    Secret sudo user can use gkick now
    
    ;__;

--- tg_bot/modules/global_kick.py
+++ tg_bot/modules/global_kick.py
@@ -4,7 +4,7 @@ from typing import List, Optional
 from telegram.error import BadRequest, TelegramError
 from telegram.ext import run_async, CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
-from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, STRICT_GBAN
+from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SECRET_SUDO_USERS, SUPPORT_USERS, STRICT_GBAN
 from tg_bot.modules.helper_funcs.chat_status import user_admin, is_user_admin
 from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
 from tg_bot.modules.helper_funcs.filters import CustomFilters
@@ -48,6 +48,9 @@ def gkick(bot: Bot, update: Update, args: List[str]):
     if int(user_id) in SUDO_USERS or int(user_id) in SUPPORT_USERS:
         message.reply_text("OHHH! Someone's trying to gkick a sudo/support user! *Grabs popcorn*")
         return
+    if int(user_id) in SECRET_SUDO_USERS:
+        message.reply_text("Sar He is a secret sudo user *Grabs Ak47*")
+        return
     if int(user_id) == OWNER_ID:
         message.reply_text("Wow! Someone's so noob that he want to gkick my owner! *Grabs Potato Chips*")
         return
@@ -69,5 +72,5 @@ def gkick(bot: Bot, update: Update, args: List[str]):
             pass
 
 GKICK_HANDLER = CommandHandler("gkick", gkick, pass_args=True,
-                              filters=CustomFilters.sudo_filter | CustomFilters.support_filter)
+                              filters=CustomFilters.sudo_filter | CustomFilters.support_filter | CustomFilters.secret_sudo_filter)
 dispatcher.add_handler(GKICK_HANDLER)                              
