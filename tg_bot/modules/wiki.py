d021f994bf0755a8aadba9f8fb82cd230a683e55
Baalaji <baalajimaestro@computer4u.com>
2018-09-01 19:41:00 +0530

    Fix wiki
    Signed-off-by: Yasir-siddiqui <www.mohammad.yasir.s@gmail.com>
    
    Fix typo

--- tg_bot/modules/wiki.py
+++ tg_bot/modules/wiki.py
@@ -3,14 +3,16 @@ from telegram import Update, Bot
 from telegram.ext import run_async, Filters
 from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler
+
 def wiki(bot: Bot, update: Update):
-        query = update.effective_message.text.split(None, 1)
-        result = '**Search:**\n`' + query + '`\n\n**Result:**\n`' + str(wikipedia.summary(match))
+        query = str(update.effective_message.text[6:])
+        result = '**Search:**\n' + query + '\n\n**Result:**\n' + str(wikipedia.summary(query))
         update.effective_message.reply_markdown(result)
 
 __help__ = """
- - /wiki: Query the Wikipedia OwO.
+ - /wiki: Query the Wikipedia
  """
 __mod_name__ = "Wikipedia Search"
+
 WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki)
 dispatcher.add_handler(WIKI_HANDLER)
