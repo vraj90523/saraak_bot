637a22d4b60de37e523961efe2833bc47ca8abf7
adi19v <45130202+adi19v@users.noreply.github.com>
2019-02-14 16:32:14 +0530

    Update special.py

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -3,45 +3,19 @@ from time import sleep
 from typing import Optional, List
 from telegram import TelegramError, Chat, Message
 from telegram import Update, Bot
+from telegram import ParseMode
 from telegram.error import BadRequest
 from telegram.ext import MessageHandler, Filters, CommandHandler
 from telegram.ext.dispatcher import run_async
 from tg_bot.modules.helper_funcs.chat_status import is_user_ban_protected, bot_admin
+from telegram.utils.helpers import escape_markdown
 
 import tg_bot.modules.sql.users_sql as sql
 import random, re
-from tg_bot import dispatcher, OWNER_ID, LOGGER
+from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, LOGGER
 from tg_bot.modules.helper_funcs.filters import CustomFilters
 
 USERS_GROUP = 4
-
-
-@run_async
-def quickscope(bot: Bot, update: Update, args: List[int]):
-    if args:
-        chat_id = str(args[1])
-        to_kick = str(args[0])
-    else:
-        update.effective_message.reply_text("You don't seem to be referring to a chat/user")
-    try:
-        bot.kick_chat_member(chat_id, to_kick)
-        update.effective_message.reply_text("Attempted banning " + to_kick + " from" + chat_id)
-    except BadRequest as excp:
-        update.effective_message.reply_text(excp.message + " " + to_kick)
-
-
-@run_async
-def quickunban(bot: Bot, update: Update, args: List[int]):
-    if args:
-        chat_id = str(args[1])
-        to_kick = str(args[0])
-    else:
-        update.effective_message.reply_text("You don't seem to be referring to a chat/user")
-    try:
-        bot.unban_chat_member(chat_id, to_kick)
-        update.effective_message.reply_text("Attempted unbanning " + to_kick + " from" + chat_id)
-    except BadRequest as excp:
-        update.effective_message.reply_text(excp.message + " " + to_kick)
 
 
 @run_async
@@ -107,48 +81,60 @@ def getlink(bot: Bot, update: Update, args: List[int]):
 def leavechat(bot: Bot, update: Update, args: List[int]):
     if args:
         chat_id = int(args[0])
-        bot.leaveChat(chat_id)
     else:
-        update.effective_message.reply_text("You don't seem to be referring to a chat")
+        update.effective_message.reply_text("You do not seem to be referring to a chat!")
+    try:
+        chat = bot.getChat(chat_id)
+        titlechat = bot.get_chat(chat_id).title
+        bot.sendMessage(chat_id, "Goodbye all 😁")
+        bot.leaveChat(chat_id)
+        update.effective_message.reply_text("I left group {}".format(titlechat))
 
-__help__ = """  
-**Owner only:**
-- /getlink **chatid**: Get the invite link for a specific chat.
-- /banall: Ban all members from a chat
-- /leavechat **chatid** : leave a chat
-**Sudo/owner only:**
-- /quickscope **chatid** **userid**: Ban user from chat.
-- /quickunban **chatid** **userid**: Unban user from chat.
-- /snipe **chatid** **string**: Make me send a message to a specific chat.
-- /rban **chatid** **userid** remotely ban a user from a chat
-- /runban **chatid** **userid** remotely unban a user from a chat
-- /Stats: check bot's stats
-- /chatlist: get chatlist 
-- /gbanlist: get gbanned users list
-- /gmutelist: get gmuted users list
-- Chat bans via /restrict chat_id and /unrestrict chat_id commands
-**Support user:**
-- /Gban : Global ban a user
-- /Ungban : Ungban a user
-- /Gmute : Gmute a user
-- /Ungmute : Ungmute a user
-Sudo/owner can use these commands too.
-**Users:**
-- /listsudo Gives a list of sudo users
-- /listsupport gives a list of support users
-"""
-__mod_name__ = "Special"
+    except BadRequest as excp:
+        if excp.message == "Chat not found":
+            update.effective_message.reply_text("It looks like I've been kicked out of the group :p")
+        else:
+            return
+
+
+@run_async
+def slist(bot: Bot, update: Update):
+    message = update.effective_message
+    text1 = "My sudo users are:"
+    text2 = "My support users are:"
+    for user_id in SUDO_USERS:
+        try:
+            user = bot.get_chat(user_id)
+            name = "[{}](tg://user?id={})".format(user.first_name + (user.last_name or ""), user.id)
+            if user.username:
+                name = escape_markdown("@" + user.username)
+            text1 += "\n - {}".format(name)
+        except BadRequest as excp:
+            if excp.message == 'Chat not found':
+                text1 += "\n - ({}) - not found".format(user_id)
+    for user_id in SUPPORT_USERS:
+        try:
+            user = bot.get_chat(user_id)
+            name = "[{}](tg://user?id={})".format(user.first_name + (user.last_name or ""), user.id)
+            if user.username:
+                name = escape_markdown("@" + user.username)
+            text2 += "\n - {}".format(name)
+        except BadRequest as excp:
+            if excp.message == 'Chat not found':
+                text2 += "\n - ({}) - not found".format(user_id)
+    message.reply_text(text1 + "\n", parse_mode=ParseMode.MARKDOWN)
+    message.reply_text(text2 + "\n", parse_mode=ParseMode.MARKDOWN)
+
 
 SNIPE_HANDLER = CommandHandler("snipe", snipe, pass_args=True, filters=CustomFilters.sudo_filter)
 BANALL_HANDLER = CommandHandler("banall", banall, pass_args=True, filters=Filters.user(OWNER_ID))
-QUICKSCOPE_HANDLER = CommandHandler("quickscope", quickscope, pass_args=True, filters=CustomFilters.sudo_filter)
-QUICKUNBAN_HANDLER = CommandHandler("quickunban", quickunban, pass_args=True, filters=CustomFilters.sudo_filter)
 GETLINK_HANDLER = CommandHandler("getlink", getlink, pass_args=True, filters=Filters.user(OWNER_ID))
 LEAVECHAT_HANDLER = CommandHandler("leavechat", leavechat, pass_args=True, filters=Filters.user(OWNER_ID))
+SLIST_HANDLER = CommandHandler("slist", slist,
+                           filters=CustomFilters.sudo_filter | CustomFilters.support_filter)
 
 dispatcher.add_handler(SNIPE_HANDLER)
 dispatcher.add_handler(BANALL_HANDLER)
-dispatcher.add_handler(QUICKSCOPE_HANDLER)
-dispatcher.add_handler(QUICKUNBAN_HANDLER)
 dispatcher.add_handler(GETLINK_HANDLER)
 dispatcher.add_handler(LEAVECHAT_HANDLER)
+dispatcher.add_handler(SLIST_HANDLER)
